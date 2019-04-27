97905a784bbd818809f8746036863ea4a3e3a110
langjiapeng <2722700505@qq.com>
2019-04-27 13:57:23 +0800

    1

---
+++ header.js
@@ -0,0 +1,26 @@
+//header
+document.writeln("<div id=\'kratos-header\'><header id=\'kratos-header-section\'><div class=\'container\'<div class=\'nav-header\'><nav id=\'kratos-menu-wrap\' class=\'menu-main-container\'>");
+
+
+//首页
+document.writeln("<ul id=\'kratos-primary-menu\' class=\'sf-menu\'><li class=\'current-menu-item\'><a href=\'https://langjiapeng.github.io\' aria-current=\'page\'><i class=\'fa fa-home\'></i>首页</a></li>");
+
+
+//页面
+document.writeln("<li><a><i class=\'fa fa-dashboard\'></i>页面</a>");
+document.writeln("<ul class=\'sub-menu\'><li><a href=\'https://langjiapeng.github.io/down.html\'>下载页</a></li>");
+document.writeln("<li><a href=\'https://langjiapeng.github.io/info.html\'>信息页</a></li>");
+document.writeln("<li><a href=\'https://langjiapeng.github.io/res.html\'>资源页</a></li></ul></li>");
+
+
+
+//其他
+document.writeln("<li><a href=\'https://langjiapeng.github.io/tools.html\'><i class=\'fa fa-cogs\'></i>工具</a></li>");
+document.writeln("<li><a href=\'https://langjiapeng.github.io/about.html\'><i class=\'fa fa-rocket\'></i>关于</a></li>");
+document.writeln("</ul></nav></div></div></header><div>");
+
+
+document.writeln("<div class=\'kratos-start kratos-hero-2\'><div class=\'kratos-overlay\'></div><div class=\'kratos-cover kratos-cover_2 text-center\'><div class=\'desc desc2 animate-box\'>");
+
+
+document.writeln("<a href=\'https://langjiapeng.github.io\'><h2>Lang's Blog</h2><br><span id=\'hitokoto\'><script src=\'https://api.fczbl.vip/hitokoto/?charset=utf-8&encode=js\'></script><script>hitokoto()</script></span></a></div></div></div>");
