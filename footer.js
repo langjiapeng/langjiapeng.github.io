4b2c0a477d3b1845cc8a8ef928fe223697fcad6e
langjiapeng <2722700505@qq.com>
2019-04-27 21:54:49 +0800

    1

--- footer.js
+++ footer.js
@@ -1,10 +1,14 @@
-//页脚
-document.writeln("<footer><div id=\'footer\' style=\'background:rgba(35,40,45,1)\'><div class=\'container\'><div class=\'row\'><div class=\'col-md-6 col-md-offset-3 footer-list text-center\'>");
-
-
-
-document.writeln("<p> © 2019 <a href=\'https://langjiapeng.github.io\'>Lang&#039;s Blog</a>. All Rights Reserved.</p>");
-
-
-
-document.writeln("</div></div></div><div class=\'cd-tool text-center\'><div class=\'gotop-box\'><div class=\'gotop-btn\'><span class=\'fa fa-chevron-up\'></span></div></div></div></div></footer>");
+document.writeln('<footer>'+
+'		  <div id="footer" style="background:rgba(35,40,45,1)">'+
+'			<div class="container">'+
+'			  <div class="row">'+
+'				<div class="col-md-6 col-md-offset-3 footer-list text-center">'+
+'				  <p> © 2019 <a href="https://langjiapeng.github.io">Lang&#039;s Blog</a>. All Rights Reserved.</p>'+
+'				</div>'+
+'			  </div>'+
+'			</div>'+
+'			<div class="cd-tool text-center">'+
+'			  <div class="gotop-box"><div class="gotop-btn"><span class="fa fa-chevron-up"></span></div></div>'+
+'			</div>'+
+'		  </div>'+
+'		</footer>');
