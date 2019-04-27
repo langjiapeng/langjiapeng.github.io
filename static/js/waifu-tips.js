5931bb76d296179f5676035669a2d651dc6101fa
langjiapeng <2722700505@qq.com>
2019-04-27 15:15:22 +0800

    1

--- static/js/waifu-tips.js
+++ static/js/waifu-tips.js
@@ -4,43 +4,6 @@ $(document).on('copy',function(){
 $('.waifu-tool .fa-home').click(function(){
     try{if(typeof(ajax)==="function") ajax(window.location.protocol+'//'+window.location.hostname+'/',"pagelink"); else window.location = window.location.protocol+'//'+window.location.hostname+'/';}catch(e){}
 });
-var model_p = 22;
-$('.waifu-tool .fa-drivers-license-o').click(function(){
-    loadlive2d('live2d',xb.thome+'/inc/model/api.php?p='+model_p+'&model=rand');
-    if(model_p===22){
-        model_p = 33;
-        showMessage('33援交有点累了，现在该我上场了',4000);
-    }else{
-        model_p = 22;
-        showMessage('我又回来了！',4000);
-    }
-});
-$('.waifu-tool .fa-comments').click(function(){
-    showHitokoto();
-});
-$('.waifu-tool .fa-street-view').click(function (){
-    if(model_p===22) loadlive2d('live2d',xb.thome+'/inc/model/api.php?p=33&model=rand'); else loadlive2d('live2d',xb.thome+'/inc/model/api.php?p=22&model=rand');
-    showMessage('我的新衣服好看嘛',4000);
-});
-$('.waifu-tool .fa-info-circle').click(function (){
-    window.open('https://www.fczbl.vip/946.html');
-});
-$('.waifu-tool .fa-camera').click(function (){
-    showMessage('照好了嘛，是不是很可爱呢？',5000);
-    window.Live2D.captureName = model_p+'.png';
-    window.Live2D.captureFrame = true;
-});
-$('.waifu-tool .fa-close').click(function(){
-    sessionStorage.setItem('waifu-dsiplay','none');
-    showMessage('愿你有一天能与重要的人重逢',2000);
-    window.setTimeout(function(){$('.waifu').hide();},1000);
-});
-loadlive2d('live2d',xb.thome+'/inc/model/api.php?p=33&model=default');
-function showHitokoto(){
-    $.post("https://api.fczbl.vip/hitokoto/",function(result){
-        showMessage(result);
-    });
-}
 function showMessage(a,b){
     if(b==null) b = 10000;
     jQuery(".waifu-tips").hide().stop();
@@ -73,23 +36,35 @@ function showMessage(a,b){
             text = '嗨~ 快来逗我玩吧！';
         }
     }else{
-        if(document.referrer!==''){
+        if (document.referrer !== '')
+		{
             var referrer = document.createElement('a');
             referrer.href = document.referrer;
             var domain = referrer.hostname.split('.')[1];
-            if(window.location.hostname==referrer.hostname){
-                text = '欢迎阅读<span style="color:#0099cc;">『'+document.title.split(' - ')[0]+'』</span>';
-            }else if(domain=='baidu') {
-                text = 'Hello! 来自 百度搜索 的朋友<br>你是搜索 <span style="color:#0099cc;">'+referrer.search.split('&wd=')[1].split('&')[0]+'</span> 找到的我吗？';
-            }else if(domain=='so') {
-                text = 'Hello! 来自 360搜索 的朋友<br>你是搜索 <span style="color:#0099cc;">'+referrer.search.split('&q=')[1].split('&')[0]+'</span> 找到的我吗？';
-            }else if(domain=='google') {
-                text = 'Hello! 来自 谷歌搜索 的朋友<br>欢迎阅读<span style="color:#0099cc;">『'+document.title.split(' - ')[0]+'』</span>';
-            }else{
-                text = 'Hello! 来自 <span style="color:#0099cc;">'+referrer.hostname+'</span> 的朋友';
+            if (window.location.hostname == referrer.hostname)
+			{
+                text = '欢迎阅读<span style="color:#0099cc;">『' + document.title.split(' - ')[0] + '』</span>';
             }
-        }else{
-            text = '欢迎阅读<span style="color:#0099cc;">『'+document.title.split(' - ')[0]+'』</span>';
+			else if (domain == 'baidu')
+			{
+                text = 'Hello! 来自 百度搜索 的朋友<br>你是搜索 <span style="color:#0099cc;">' + referrer.search.split('&wd=')[1].split('&')[0] + '</span> 找到的我吗？';
+            }
+			else if (domain == 'so')
+			{
+                text = 'Hello! 来自 360搜索 的朋友<br>你是搜索 <span style="color:#0099cc;">' + referrer.search.split('&q=')[1].split('&')[0] + '</span> 找到的我吗？';
+            }
+			else if (domain == 'google')
+			{
+                text = 'Hello! 来自 谷歌搜索 的朋友<br>欢迎阅读<span style="color:#0099cc;">『' + document.title.split(' - ')[0] + '』</span>';
+            }
+			else
+			{
+                text = 'Hello! 来自 <span style="color:#0099cc;">' + referrer.hostname + '</span> 的朋友';
+            }
+        }
+		else
+		{
+            text = '欢迎阅读<span style="color:#0099cc;">『' + document.title.split(' - ')[0] + '』</span>';
         }
     }
     $(".waifu").animate({top:$(window).height()-250},800);
@@ -97,115 +72,6 @@ function showMessage(a,b){
 })();
 $(window).resize(function(){
     $(".waifu").css('top',window.innerHeight-250);
-});
-$("#live2d").mouseover(function(){
-    msgs = ["你要干嘛呀？","鼠…鼠标放错地方了！","喵喵喵？","萝莉控是什么呀？","怕怕","你看到我的小熊了吗"];
-    var i = Math.floor(Math.random()*msgs.length);
-    showMessage(msgs[i]);
-});
-jQuery(document).ready(function($){
-    $('.search-box').mouseover(function(){
-        showMessage('找不到想要的？试试搜索吧！');
-    });
-    $('#search').focus(function(){
-        showMessage('输入你想搜索的关键词再按Enter键就可以搜索啦!');
-    });
-    $('.desc a h2,.desc a span,.color-logo a,.back-index,.waifu-tool .fa-home,#kratos-primary-menu .fa-home').mouseover(function(){
-        showMessage('点它就可以回到首页啦！');
-    });
-    $('#footer p a i.fa-weibo').mouseover(function(){
-        showMessage('微博？求关注喵！');
-    });
-    $('#footer p a i.fa-envelope').mouseover(function(){
-        showMessage('邮件我会及时回复的！');
-    });
-    $('#footer p a i.fa-twitter').mouseover(function(){
-        showMessage('Twitter?好像是不存在的东西?');
-    });
-    $('#footer p a i.fa-facebook-official').mouseover(function(){
-        showMessage('emmm...FB已经好久没上了...');
-    });
-    $('#footer p a i.fa-github').mouseover(function(){
-        showMessage('GayHub！我是新手！');
-    });
-    $('#wechat-img').mouseover(function(){
-        showMessage('这是我的微信二维码~');
-    });
-    $('.gotop-box').mouseover(function(){
-        showMessage('要回到开始的地方么？');
-    });
-    $('.waifu-tool .fa-comments').mouseover(function(){
-        showMessage('猜猜我要说些什么？');
-    });
-    $('.waifu-tool .fa-drivers-license-o').mouseover(function(){
-        if(model_p===22) showMessage('要见见我的姐姐嘛'); else showMessage('什么？我的服务不好，要33回来？');
-    });
-    $('.waifu-tool .fa-street-view').mouseover(function(){
-        showMessage('喜欢换装play吗？');
-    });
-    $('.waifu-tool .fa-camera').mouseover(function(){
-        showMessage('你要给我拍照呀？一二三～茄子～');
-    });
-    $('.waifu-tool .fa-info-circle').mouseover(function(){
-        showMessage('想知道更多关于我的事么？');
-    });
-    $('.waifu-tool .fa-close').mouseover(function(){
-        showMessage('到了要说再见的时候了吗');
-    });
-    $(document).on("click","h2 a",function(){
-        showMessage('加载<span style="color:#0099cc;">'+$(this).text()+'</span>中...请稍候',600);
-    });
-    $(document).on("mouseover","h2 a",function(){
-        showMessage('要看看<span style="color:#0099cc;">'+$(this).text()+'</span>么？');
-    });
-    $(document).on("mouseover",".prev",function(){
-        showMessage('要翻到上一页吗?');
-    });
-    $(document).on("mouseover",".next",function(){
-        showMessage('要翻到下一页吗?');
-    });
-    $(document).on("mouseover",".kratos-post-content a",function(){
-        showMessage('去 <span style="color:#0099cc;">'+$(this).text()+'</span> 逛逛吧');
-    });
-    $(document).on("mouseover","#submit",function(){
-        showMessage('呐 首次评论需要审核，请耐心等待哦~');
-    });
-    $(document).on("mouseover",".OwO-logo",function(){
-        showMessage('要来一发表情吗？');
-    });
-    $(document).on("mouseover",".nav-previous",function(){
-        showMessage('点它可以后退哦！');
-    });
-    $(document).on("mouseover",".nav-next",function(){
-        showMessage('点它可以前进哦！');
-    });
-    $(document).on("mouseover",".comment-reply-link",function(){
-        showMessage('要说点什么吗');
-    });
-    $(document).on("mouseover",".Donate",function(){
-        showMessage('要打赏我嘛？好期待啊~');
-    });
-    $(document).on("mouseover",".Love",function(){
-        showMessage('我是不是棒棒哒~快给我点赞吧！');
-    });
-    $(document).on("mouseover",".must-log-in",function(){
-        showMessage('登录才可以继续哦~');
-    });
-    $(document).on("mouseover",".Share",function(){
-        showMessage('好东西要让更多人知道才行哦');
-    });
-    $(document).on("click","#author",function(){
-        showMessage("留下你的尊姓大名！");
-    });
-    $(document).on("click","#email",function(){
-        showMessage("留下你的邮箱，不然就是无头像人士了！");
-    });
-    $(document).on("click","#url",function(){
-        showMessage("快快告诉我你的家在哪里，好让我去参观参观！");
-    });
-    $(document).on("click","#comment",function(){
-        showMessage("一定要认真填写喵~");
-    });
 });
 jQuery(document).ready(function($){
     window.setInterval(function(){showMessage(showHitokoto());},25000);
