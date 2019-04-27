67debf9b71839d36fd1af138dbf2dc5f0191bba3
langjiapeng <2722700505@qq.com>
2019-04-27 14:20:18 +0800

    1

---
+++ footer.js
@@ -0,0 +1,10 @@
+//页脚
+document.writeln("<footer><div id=\'footer\' style=\'background:rgba(35,40,45,1)\'><div class=\'container\'><div class=\'row\'><div class=\'col-md-6 col-md-offset-3 footer-list text-center\'>");
+
+
+
+document.writeln("<p> © 2019 <a href=\'https://langjiapeng.github.io\'>Lang&#039;s Blog</a>. All Rights Reserved.</p>");
+
+
+
+document.writeln("</div></div></div><div class=\'cd-tool text-center\'><div class=\'gotop-box\'><div class=\'gotop-btn\'><span class=\'fa fa-chevron-up\'></span></div></div></div></div></footer>");
