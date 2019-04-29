ca6dc9cd9c305fbea8d9ddb752e05ffa6772069e
langjiapeng <2722700505@qq.com>
2019-04-29 21:41:12 +0800

    1

--- header.js
+++ header.js
@@ -2,18 +2,18 @@ document.writeln('<div id="kratos-header">'+
 '		  <header id="kratos-header-section">'+
 '			<div class="container">'+
 '			  <div class="nav-header">'+
-'				<nav id="kratos-menu-wrap" class="menu-main-container"><ul id="kratos-primary-menu" class="sf-menu"><li class="current-menu-item"><a href="https://langjiapeng.github.io" aria-current="page"><i class="fa fa-home"></i>首页</a></li>'+
-'					<li><a><i class="fa fa-dashboard"></i>页面</a>'+
+'				<nav id="kratos-menu-wrap" class="menu-main-container"><ul id="kratos-primary-menu" class="sf-menu">'+
+'                   <li class="current-menu-item"><a href="https://langjiapeng.github.io" aria-current="page"><i class="fa fa-home"></i> 首页</a></li>'+
+'					<li><a><i class="fa fa-dashboard"></i> 页面</a>'+
 '					  <ul class="sub-menu">'+
 '						<li><a href="/down.html">下载页</a></li>'+
 '						<li><a href="/info.html">信息页</a></li>'+
 '						<li><a href="/res.html">资源页</a></li>'+
 '					  </ul>'+
 '					</li>'+
-'					<li><a href="/tools.html"><i class="fa fa-cogs"></i>工具</a></li>'+
-'                   <li><a href="/page/links.html"><i class="fa fa-mars"></i>链接</a></li>'+
-
-'					<li><a href="/about.html"><i class="fa fa-rocket"></i>关于</a></li>'+
+'					<li><a href="/tools.html"><i class="fa fa-cogs"></i> 工具</a></li>'+
+'                   <li><a href="/page/links.html"><i class="fa fa-mars"></i> 链接</a></li>'+
+'					<li><a href="/about.html"><i class="fa fa-rocket"></i> 关于</a></li>'+
 '				  </ul></nav></div>'+
 '			</div>'+
 '		  </header>'+
